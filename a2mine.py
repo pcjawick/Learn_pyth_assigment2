96ce09856dda5fdd0b523a0cfbbe4f9f62fa821f
pcjawick <pcjawick@gmail.com>
2013-09-21 23:08:36 +0200

    TEste top tre i IDLE
    Manlger litt på count nucletides

--- a2mine.py
+++ a2mine.py
@@ -8,8 +8,9 @@ def get_length(dna):
     >>> get_length('ATCG')
     4
     """
-
-    return len('dna')
+    #if len('dna') != 0
+    return len(dna)
+    #elseif print 'You didnt enter anything'
 
 
 def is_longer(dna1, dna2):
@@ -23,9 +24,10 @@ def is_longer(dna1, dna2):
     >>> is_longer('ATCG', 'ATCGGA')
     False
     """
-    
+    #if len('dna1') or len('dna2') != 0
+    return len(dna1) > len(dna2)
+    #elseif return ''
 
-    return len('dna1') > len('dna2')
 
 def count_nucleotides(dna, nucleotide):
     """ (str, str) -> int
@@ -37,8 +39,13 @@ def count_nucleotides(dna, nucleotide):
     >>> count_nucleotides('ATCTA', 'G')
     0
     """
+    #count_nucleotides(dna, nucleotide):
+    number = 0
+    for nucleotide in dna:
+        number += 1
+        print (number)
 
-    return 'nucleotide' in 'dna'
+
 
 def contains_sequence(dna1, dna2):
     """ (str, str) -> bool
@@ -56,25 +63,25 @@ def contains_sequence(dna1, dna2):
 
 """four more defs from here"""   
 
-def is_valid_sequence 
+#def is_valid_sequence 
 '''str-> bool
 returns true if only A,T, C, G is represented 
 
 '''
-def insert_sequence
+#def insert_sequence
 '''(str, str, int) -> str
 Inserts sequense at a given index
 
 '''
 
 
-def get_complement  
+#def get_complement  
 '''(str) -> str
 Returns complimentary nukleotide for A(=T), T(=A), C(=G), G(=C) 
 '''
 
 
-def get_complementary_sequence
+#def get_complementary_sequence
 '''(str) -> str
 eturns complimentary nukleotide for A(=T), T(=A), C(=G), G(=C) in sequense
 '''
