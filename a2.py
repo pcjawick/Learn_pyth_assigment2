a7b963536755e7567a3adc2b0fb1c0cd960a17bf
pcjawick <pcjawick@gmail.com>
2013-09-22 23:28:18 +0200

    testet everything workx in idle

--- a2.py
+++ a2.py
@@ -67,29 +67,80 @@ def contains_sequence(dna1, dna2):
     return 'false'
 
 """All above tested and OK. Score 12/24 as of sun 22.9"""   
+#is valid is tested and checked in IDLE:-)
+def is_valid_sequence(dna):
+    """str-> bool
+    returns true if only A,T, C, G is represented"""
+    
 
-#def is_valid_sequence 
-'''str-> bool
-returns true if only A,T, C, G is represented 
+    n = 0
+    for u in dna:
+        if u not in 'ATCG':
+            n = n + 1
 
-'''
-#def insert_sequence
-'''(str, str, int) -> str
-Inserts sequense at a given index
+    
+    return (n == 0)
 
-'''
+def insert_sequence(nuc1,nuc2,num):#TEsted and workx
+    '''(str, str, int) -> str
+    Inserts sequense at a given index
+
+    insert_sequence('ATCG','TTT', -2)
+    'ATTTTCG'
+
+    '''
+    dns = ''
 
 
-#def get_complement  
-'''(str) -> str
-Returns complimentary nukleotide for A(=T), T(=A), C(=G), G(=C) 
-'''
+    if abs(num) < (len(nuc1)):
+            dns = (nuc1[0:num] + nuc2 + nuc1[(num):(len(nuc1)+len(nuc2))])
+            return dns
+
+    return 'Indexerror'
 
 
-#def get_complementary_sequence
-'''(str) -> str
-eturns complimentary nukleotide for A(=T), T(=A), C(=G), G(=C) in sequense
-'''
+
+
+def get_complement(dna):  #Tested and si valid in IDLE
+    '''(str) -> str
+    Returns complimentary nukleotide for A(=T), T(=A), C(=G), G(=C) 
+    get_complement('A')
+    'T'
+    '''
+    complementary = ''
+
+    for u in dna:
+        if u in 'A':
+            complementary = 'T'
+        if u in 'T':
+            complementary = 'A'
+        if u in 'C':
+            complementary = 'G'
+        if u in 'G':
+            complementary = 'C'
+
+    return complementary
+
+def get_complementary_sequence(dna):#Tested and si valid in IDLE
+    '''(str) -> str
+    
+    Returns complimentary nukleotide for A(=T), T(=A), C(=G), G(=C) in sequense
+    get_complementary_sequence('ATCTA')
+    'TAGAT'
+    '''
+    complementary = ''
+
+    for u in dna:
+        if u in 'A':
+            complementary += 'T'
+        if u in 'T':
+            complementary += 'A'
+        if u in 'C':
+            complementary += 'G'
+        if u in 'G':
+            complementary += 'C'
+
+    return complementary
 
 
 
